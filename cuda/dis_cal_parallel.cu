e6d239c94020e27ad3069dff4d23c93735233863
gt <jack139@gmail.com>
2021-07-01 21:46:52 +0800

    改为float

--- cuda/dis_cal_parallel.cu
+++ cuda/dis_cal_parallel.cu
@@ -1,17 +1,18 @@
 #include <sys/time.h>
-#include<iostream>                                                                                                       
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <iostream>   
+#include <iomanip>                                                                                                    
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 using namespace std;
 
 const int N = 100;
 const int D = 10;
-const int MAX = 10;
+const float MAX = 10000.0;
 
-__global__ void cal_dis(int *train_data, int *test_data, int *dis,int pitch)
+__global__ void cal_dis(float *train_data, float *test_data, float *dis,int pitch)
 {
 	int tid = blockIdx.x;
 	if(tid<N)
@@ -27,44 +28,44 @@ __global__ void cal_dis(int *train_data, int *test_data, int *dis,int pitch)
 	}
 }
  
-void print(int data[][D])
+void print(float data[][D])
 {
 	cout<<"training data:"<<endl;
  	for(int i=0;i<N;i++)
 	{
 		for(int j=0;j<D;j++)
 		{
-			cout<<*(*(data+i)+j)<<" ";		
+			cout<< fixed << setprecision(6)<<*(*(data+i)+j)<<" ";		
 		}
 		cout<<endl;
 	}
 }
  
-void print(int *data,int n)
+void print(float *data,int n)
 {
 	for(int i=0;i<n;i++)
 	{
-		cout<<data[i]<<" ";
+		cout<< fixed << setprecision(6)<<data[i]<<" ";
 	}
 	cout<<endl;
 }
  
 int main()
 {
-	int h_train_data[N][D] , h_test_data[D] , distance[N];
+	float h_train_data[N][D] , h_test_data[D] , distance[N];
  
-	int *d_train_data , *d_test_data , *d_dis;
+	float *d_train_data , *d_test_data , *d_dis;
  
  	struct timeval t1,t2;
     double timeuse;
 
 	size_t pitch_d;
-	size_t pitch_h = D * sizeof(int) ;
+	size_t pitch_h = D * sizeof(float) ;
  
 	//allocate memory on GPU 
 	cudaMallocPitch( &d_train_data , &pitch_d , D * sizeof(float) , N ); 
-	cudaMalloc( (void**)&d_test_data ,  D*sizeof(int) );
-	cudaMalloc( (void**)&d_dis , 		  N*sizeof(int) );
+	cudaMalloc( (void**)&d_test_data ,  D*sizeof(float) );
+	cudaMalloc( (void**)&d_dis , N*sizeof(float) );
  
 	//initialize training data
 	srand( (unsigned)time(NULL) );
@@ -72,7 +73,7 @@ int main()
 	{
 		for( int j=0;j<D;j++)
 		{
-			h_train_data[i][j] = rand()%MAX;
+			h_train_data[i][j] = rand()/MAX;
 		}
 	}
 	print(h_train_data);
@@ -80,7 +81,7 @@ int main()
 	//initialize testing data
 	for( int j=0;j<D;j++ )
 	{
-	  	h_test_data[j] = rand() % MAX;
+	  	h_test_data[j] = rand()/MAX;
 	}
 	cout<<"testing data:"<<endl;
 	print(h_test_data,D);
@@ -88,14 +89,14 @@ int main()
 	gettimeofday(&t1,NULL);
 
 	//copy training and testing data from host to device
-	cudaMemcpy2D( d_train_data , pitch_d , h_train_data , pitch_h , D * sizeof(int) , N , cudaMemcpyHostToDevice );
-	cudaMemcpy( d_test_data,  h_test_data ,  D*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy2D( d_train_data , pitch_d , h_train_data , pitch_h , D * sizeof(float) , N , cudaMemcpyHostToDevice );
+	cudaMemcpy( d_test_data,  h_test_data ,  D*sizeof(float), cudaMemcpyHostToDevice);
  
 	//calculate the distance
 	cal_dis<<<N,1>>>( d_train_data,d_test_data,d_dis,pitch_d );
  
 	//copy distance data from device to host
-	cudaMemcpy( distance , d_dis  , N*sizeof(int) , cudaMemcpyDeviceToHost);
+	cudaMemcpy( distance , d_dis  , N*sizeof(float) , cudaMemcpyDeviceToHost);
 
 	gettimeofday(&t2,NULL);
  
@@ -107,7 +108,7 @@ int main()
 	cudaFree(d_dis);
  
  	timeuse = (t2.tv_sec - t1.tv_sec) + (double)(t2.tv_usec - t1.tv_usec)/1000000.0;
-  	cout << "[ time taken: " << timeuse << "s ]" << endl;
+  	cout << "[ time taken: " << fixed << setprecision(6) << timeuse << "s ]" << endl;
 
 	return 0;
 }  
