1fd8906ea08e5cbb2e8844d21d3569b5a87c8319
gt <jack139@gmail.com>
2021-07-02 13:59:42 +0800

    fix cuda code

--- cuda/dis_cal_parallel.cu
+++ cuda/dis_cal_parallel.cu
@@ -8,11 +8,11 @@
 
 using namespace std;
 
-const int N = 100;
+const int N = 10;
 const int D = 10;
-const float MAX = 10000.0;
 
-__global__ void cal_dis(float *train_data, float *test_data, float *dis,int pitch)
+
+__global__ void cal_dis(double *train_data, double *test_data, double *dis,int pitch)
 {
 	int tid = blockIdx.x;
 	if(tid<N)
@@ -27,76 +27,119 @@ __global__ void cal_dis(float *train_data, float *test_data, float *dis,int pitc
 		dis[tid] = sum;
 	}
 }
- 
-void print(float data[][D])
+
+void print(double *data)
 {
 	cout<<"training data:"<<endl;
- 	for(int i=0;i<N;i++)
+	for(int i=0;i<N;i++)
 	{
 		for(int j=0;j<D;j++)
 		{
-			cout<< fixed << setprecision(6)<<*(*(data+i)+j)<<" ";		
+			cout<< fixed << setprecision(8)<<*(data+i*D+j)<<" ";
 		}
 		cout<<endl;
 	}
 }
  
-void print(float *data,int n)
+void print(double *data, int n)
 {
 	for(int i=0;i<n;i++)
 	{
-		cout<< fixed << setprecision(6)<<data[i]<<" ";
+		cout<< fixed << setprecision(8)<<data[i]<<" ";
 	}
 	cout<<endl;
 }
- 
-int main()
-{
-	float h_train_data[N][D] , h_test_data[D] , distance[N];
- 
-	float *d_train_data , *d_test_data , *d_dis;
- 
- 	struct timeval t1,t2;
-    double timeuse;
 
-	size_t pitch_d;
-	size_t pitch_h = D * sizeof(float) ;
- 
-	//allocate memory on GPU 
-	cudaMallocPitch( &d_train_data , &pitch_d , D * sizeof(float) , N ); 
-	cudaMalloc( (void**)&d_test_data ,  D*sizeof(float) );
-	cudaMalloc( (void**)&d_dis , N*sizeof(float) );
- 
-	//initialize training data
-	srand( (unsigned)time(NULL) );
-	for( int i=0;i<N;i++ )
-	{
-		for( int j=0;j<D;j++)
-		{
-			h_train_data[i][j] = rand()/MAX;
+
+int read_data(double *data_set)
+{
+	double f1;
+	const char s[2] = ",";
+	char *token, *line;
+	FILE *fp;
+
+	// 一个数字假设占20字符，目前是保留16位小数，一共18个字符
+	line = (char *)malloc(20*D*sizeof(char)); 
+
+	fp = fopen("../vector.data" , "r");
+	if(fp == NULL) {
+		perror("打开文件时发生错误");
+		return(-1);
+	}
+
+	// 读N+1行，最后1行做测试
+	for(int i=0;i<N+1;i++) {
+		if( fgets (line, 20*2048*sizeof(char), fp)!=NULL ) {
+			token = strtok(line, s);
+
+			int j = 0;
+			while (token != NULL)
+			{
+				f1 = atof(token);
+				//printf("%.16f ", f1);
+				*(data_set+i*D+j)=f1;
+
+				token = strtok(NULL, s);
+				j++;
+			}
+			//puts("");
+
+		} else {
+			break;
 		}
 	}
+
+	fclose(fp);
+
+	free(line);
+
+	return 0;
+}
+
+int main()
+{
+	double *h_train_data, *h_test_data;
+	double distance[N];
+ 
+	double *d_train_data , *d_test_data , *d_dis;
+ 
+	struct timeval t1,t2;
+	double timeuse;
+
+	h_train_data = (double *)malloc((N+1)*D*sizeof(double));
+	if (h_train_data==NULL){
+		puts("alloc memory fail!");
+		exit(-1);
+	}
+
+	size_t pitch_d;
+	size_t pitch_h = D * sizeof(double) ; 
+
+	//allocate memory on GPU 
+	cudaMallocPitch( &d_train_data , &pitch_d , D * sizeof(double) , N ); 
+	cudaMalloc( (void**)&d_test_data ,  D*sizeof(double) );
+	cudaMalloc( (void**)&d_dis , N*sizeof(double) );
+
+	//initialize training data
+	read_data(h_train_data);
 	print(h_train_data);
  
 	//initialize testing data
-	for( int j=0;j<D;j++ )
-	{
-	  	h_test_data[j] = rand()/MAX;
-	}
+	h_test_data = h_train_data+D*N;
 	cout<<"testing data:"<<endl;
 	print(h_test_data,D);
  
 	gettimeofday(&t1,NULL);
 
 	//copy training and testing data from host to device
-	cudaMemcpy2D( d_train_data , pitch_d , h_train_data , pitch_h , D * sizeof(float) , N , cudaMemcpyHostToDevice );
-	cudaMemcpy( d_test_data,  h_test_data ,  D*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy2D( d_train_data , pitch_d , h_train_data , pitch_h , D * sizeof(double) , N , cudaMemcpyHostToDevice );
+	cudaMemcpy( d_test_data,  h_test_data ,  D*sizeof(double), cudaMemcpyHostToDevice);
  
 	//calculate the distance
 	cal_dis<<<N,1>>>( d_train_data,d_test_data,d_dis,pitch_d );
  
 	//copy distance data from device to host
-	cudaMemcpy( distance , d_dis  , N*sizeof(float) , cudaMemcpyDeviceToHost);
+	cudaMemcpy( distance , d_dis  , N*sizeof(double) , cudaMemcpyDeviceToHost);
 
 	gettimeofday(&t2,NULL);
  
@@ -106,9 +149,12 @@ int main()
 	cudaFree(d_train_data);
 	cudaFree(d_test_data);
 	cudaFree(d_dis);
- 
- 	timeuse = (t2.tv_sec - t1.tv_sec) + (double)(t2.tv_usec - t1.tv_usec)/1000000.0;
-  	cout << "[ time taken: " << fixed << setprecision(6) << timeuse << "s ]" << endl;
+	free(h_train_data);
+	 
+	timeuse = (t2.tv_sec - t1.tv_sec) + (double)(t2.tv_usec - t1.tv_usec)/1000000.0;
+	cout << "[ time taken: " << fixed << setprecision(6) << timeuse << "s ]" << endl;
+
+
 
 	return 0;
 }  
--- gen_data.py
+++ gen_data.py
@@ -3,12 +3,13 @@
 from datetime import datetime
 import numpy as np
 
-dim = 2048
-n = 1000 # 100 000 000
+dim = 10
+n = 10 # 100 000 000
 
 ff = open('vector.data', 'w')
 
-for x in range(n):
+# 多生成1个，用最后1个与其他进行比较计算
+for x in range(n+1):
 	x = np.random.random(dim)
 	xs = ','.join(['%.16f'%i for i in x.tolist()])+'\n'
 	ff.write(xs)
