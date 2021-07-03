6d44eaa8cde4bdb3100d3708a3138fc40195ab68
gt <jack139@gmail.com>
2021-07-03 20:16:20 +0800

    update

--- cuda/dis_cal_parallel.cu
+++ cuda/dis_cal_parallel.cu
@@ -9,13 +9,14 @@ using namespace std;
 
 const int D = 2048;
 const int N1 = 10000; // 数据文件条数
-const int D1 = 80; // 数据重复倍数，方便模拟海量数据
+const int D1 = 90; // 数据重复倍数，方便模拟海量数据
 const long N = N1*D1;
 
 
 __global__ void cal_dis(float *train_data, float *test_data, float *dis, int pitch)
 {
-	long tid = blockIdx.x;
+	//long tid = blockIdx.x;
+	long tid = threadIdx.x + blockIdx.x * blockDim.x;
 	if(tid<N)
 	{
 		float temp = 0.0;
@@ -119,11 +120,25 @@ int main()
  
 	float *d_train_data , *d_test_data , *d_dis;
  
-	float time1;
+	float time1, time2;
 
-	cudaEvent_t start1, stop1;
+
+	// 显示GPU资源
+	int dev = 0;
+    cudaDeviceProp devProp;
+    cudaGetDeviceProperties(&devProp, dev);
+    std::cout << "使用GPU device " << dev << ": " << devProp.name << std::endl;
+    std::cout << "SM的数量：" << devProp.multiProcessorCount << std::endl;
+    std::cout << "每个线程块的共享内存大小：" << devProp.sharedMemPerBlock / 1024.0 << " KB" << std::endl;
+    std::cout << "每个线程块的最大线程数：" << devProp.maxThreadsPerBlock << std::endl;
+    std::cout << "每个EM的最大线程数：" << devProp.maxThreadsPerMultiProcessor << std::endl;
+    std::cout << "每个EM的最大线程束数：" << devProp.maxThreadsPerMultiProcessor / 32 << std::endl;
+    //-----------
+
+	cudaEvent_t start1, stop1, stop2;
 	cudaEventCreate(&start1);
 	cudaEventCreate(&stop1); 
+	cudaEventCreate(&stop2); 
 
 	cout<<"num= "<<N<<"\tdim= "<<D<<endl;
 
@@ -157,9 +172,15 @@ int main()
 	cudaMemcpy(d_test_data, h_test_data, D*sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_dis, distance, N*sizeof(float), cudaMemcpyHostToDevice);
  
+	// 定义kernel的执行配置
+	dim3 blockSize(1);
+	dim3 gridSize((N + blockSize.x - 1) / blockSize.x);
+	printf("grid size: %d\tblock size: %d\n", gridSize.x, blockSize.x);
+	// 执行kernel
+	cal_dis<<<gridSize, blockSize>>>(d_train_data,d_test_data,d_dis,pitch_d);
 
 	//calculate the distance
-	cal_dis<<<N,1>>>(d_train_data,d_test_data,d_dis,pitch_d);
+	//cal_dis<<<N,1>>>(d_train_data,d_test_data,d_dis,pitch_d);
  
 	//copy distance data from device to host
 	cudaMemcpy(distance, d_dis, N*sizeof(float), cudaMemcpyDeviceToHost);
@@ -169,7 +190,7 @@ int main()
 	float minimum = distance[0];
 	for(long i=1;i<N;i++) if (distance[i]<minimum) minimum=distance[i];
 
-	//cudaEventRecord(stop1, 0);
+	cudaEventRecord(stop2, 0);
  
 	cout<<"distance:"<<endl;
 	//print(distance, N);
@@ -182,7 +203,8 @@ int main()
 	cout << "min= " << fixed << setprecision(8) << minimum << endl;
 
 	cudaEventElapsedTime(&time1, start1, stop1);
-	printf("[ time taken: %f ms ]\n",time1);
+	cudaEventElapsedTime(&time2, stop1, stop2);
+	printf("[ time taken: %fms %fms ]\n",time1, time2);
 
 
 	return 0;
